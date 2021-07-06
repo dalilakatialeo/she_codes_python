dba966cf615f40ba3807d39b9e48017d2472fb13
Dalila K Leo <dalilakatialeo@gmail.com>
2021-07-06 20:40:27 +1000

    first exercises with python

---
+++ user_input_playground.py
@@ -0,0 +1,14 @@
+# answer = input("How are you? ")
+# print (answer)
+
+# message = f"I'm {answer} as well, thanks for asking"
+# print(message)
+
+name = input("What is your name? ")
+# hobby = input("What is your hobby? ")
+
+# print(f"This is {name}, likes {hobby}")
+
+age = input(f"Hi, {name}, how old are you? ")
+years_until_100 = 100 - int(age)
+print (f"Wow, {name}, you'll be 100 in {years_until_100} years!")
---
+++ variables_playground.py
@@ -0,0 +1,79 @@
+#Variables
+#String
+
+dog = "Jett"
+
+# print(dog)
+
+# print(type(dog))
+
+asli = f"Hey, my dog's name is {dog}"
+# print(asli)
+
+day = "Saturday"
+message = f"Today is {day}"
+# print (message)
+# print (type(day))
+
+month = "July"
+message2 = f"Today is a {day} in {month}"
+# print (message2)
+
+#Integer Float
+apples = 5
+banana = 8
+
+fruits = apples + banana
+# print(fruits)
+# print (type(fruits))
+
+height = 1.70
+weight = 999.9
+calculation =  weight - height
+# print(calculation)
+# print(type(calculation))
+
+
+calculation = height + banana #this calculation is 9.7
+# print(calculation) 
+
+#Integers
+#Run distance in m
+
+run1_dist = 1400
+run2_dist = 1800
+
+#Addition
+total_distance = run1_dist + run2_dist
+# print(total_distance)
+
+#Floats
+#Run distance in km
+run3_dist = 1.7
+run4_dist = 1.35
+
+goal_dist = 2500
+run1_left = goal_dist - run1_dist
+# print (run1_left)
+
+#Addition
+#Division is always a float
+total_distance = run3_dist + run4_dist
+# print(total_distance)
+
+example_distance = run1_dist / run2_dist
+# print(example_distance)
+
+#Division and Multiplication
+example_distance = run1_dist / run2_dist
+# print(run1_dist / 1000)
+# print(run2_dist * 1000)
+
+run5_dist = "5000"
+# print(run5_dist + 3)
+# print(run5_dist + "3")
+# print(run5_dist * 3)
+# print(run5_dist * 3.0)
+
+print(int(run5_dist) + 3)
+print(type(str(3)))
