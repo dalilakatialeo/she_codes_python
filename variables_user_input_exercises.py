a822b470a7e472076f03477ba84d341912155028
Dalila K Leo <dalilakatialeo@gmail.com>
2021-07-08 17:31:51 +1000

    Update variables_user_input_exercises.py

--- variables_user_input_exercises.py
+++ variables_user_input_exercises.py
@@ -1,52 +1,52 @@
-#Q1
-#take two numbers from user, output their sum
+# Q1
+# take two numbers from user, output their sum
 
-# num1 = input("Enter a number: ")
-# num2 = input("Enter another number: ")
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
 
-# # user input is always a string. Need to type parse.
-# # we type parse to float, because the expected result as per example is a float.
-# # be mindful of syntax and parenthesis!
-# print(float(num1) + float(num2))
+# user input is always a string. Need to type parse.
+# we type parse to float, because the expected result as per example is a float.
+# be mindful of syntax and parenthesis!
+print(float(num1) + float(num2))
 
-#------
-#Q2
-# #take two numbers from user, output the multiplication equation!
 
-# num3 = input("Enter a number: ")
-# num4 = input("Enter another number: ")
+# Q2
+#take two numbers from user, output the multiplication equation!
 
-# # Input is always a string. Need to type parse to perform operations.
-# # Type parse to float because this is what the example shows
-# input1 = float(num3)
-# input2 = float(num4)
+num3 = input("Enter a number: ")
+num4 = input("Enter another number: ")
 
-# # Create a new variable for the multiplication result.
-# multiply = input1 * input2
+# Input is always a string. Need to type parse to perform operations.
+# Type parse to float because this is what the example shows
+input1 = float(num3)
+input2 = float(num4)
 
-# # formatting into a string
-# print(f"{input1} * {input2} = {multiply}")
+# Create a new variable for the multiplication result.
+multiply = input1 * input2
 
-# ------
-# # Q3
+# formatting into a string
+print(f"{input1} * {input2} = {multiply}")
 
-# km = input("How many kilometres? ")
 
-# # input is always a string.
-# # create new variables with new value and type parse to float so we can cover any input quantity
+# Q3
 
-# meters = float(km) * 1000
-# centimeters = float(km) * 100000
+km = input("How many kilometres? ")
 
-# # format as string and type parse to int so we don't have float as results.
+# input is always a string.
+# create new variables with new value and type parse to float so we can cover any input quantity
 
-# print(f"{km}km = {int(meters)}m")
-# print(f"{km}km = {int(centimeters)}cm")
+meters = float(km) * 1000
+centimeters = float(km) * 100000
 
-# ------
-# # Q4
+# format as string and type parse to int so we don't have float as results.
 
-# name = input("What is your name? ")
-# height = input("How tall are you (cms)? ")
+print(f"{km}km = {int(meters)}m")
+print(f"{km}km = {int(centimeters)}cm")
 
-# print(f"{name} is {height}cms tall.")
+
+# Q4
+
+name = input("What is your name? ")
+height = input("How tall are you (cms)? ")
+
+print(f"{name} is {height}cms tall.")
