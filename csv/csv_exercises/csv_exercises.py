830357253fc73d77fde09795c59e68fcddfadb0a
Dalila K Leo <dalilakatialeo@gmail.com>
2021-07-21 18:10:40 +1000

    csv exercises

--- csv/csv_exercises/csv_exercises.py
+++ csv/csv_exercises/csv_exercises.py
@@ -60,74 +60,28 @@ import csv
 
 ##########Q5
 
-# list = []
-
-# # max_velocity == max(galaxies[1])
-
-# with open("galaxies.csv") as csv_file:
-#         csv_reader = csv.reader(csv_file)
-#         for galaxy in csv_reader:
-#               list.append(galaxy) # created a list, populated with galaxy data sublists
-
-# for l in list:
-#         for i in l[1]:
-#                 new_list = [max(i) for i in l[1]]
-# print(new_list)
-# # for line in list:
-
-        
-# with open('galaxies.csv') as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter=',')
-#     slow = [None, float('inf')]
-#     fast = [None, 0]
-#     for i, row in enumerate(csv_reader):
-#         if i != 0:
-#             if int(row[1]) < slow[1]:
-#                 slow =  [row[0], int(row[1])]
-#             if int(row[1])> fast[1]:
-#                 fast = [row[0], int(row[1])]
-#     print(f'Galaxy {slow[0]} has a min velocity of {slow[1]}km/sec.')
-#     print(f'Galaxy {fast[0]} has a max velocity of {fast[1]}km/sec.')
-
-# sorted(list[1])
-#         for line in enumerate(galaxies):
-#                 if line != 0:
-#                         for i in line:
-#                                 if i[1] == min(line[1]):
-#                                         print(f"Galaxy {i[0]} has the min velocity of {i[1]} km/sec.")
-# #                 if data == max(galaxies[1]):
-#                         print(f" Galaxy {galaxies[0]} has the max velocity of {galaxies[1]} km/sec.")
-
-# print(max (galaxies[1][1]))
-
-        # for i, line in enumerate(csv_reader):
-        #         if i !=0:
-        #                 if i == min(line[1]):
-        #                         print(i)
-        #                 if i == max(line[1]):
-        #                         print(i)
-        # print(max_velocity)
-                        # print(f"Galaxy {data[0]} has the min velocity of {data[1]}km/sec.")
-#                 if d in data[1]== max(data[1]):
-#                         print(f"Galaxy {data[0]} has the max velocity of {data[1]}km/sec.")
-# # print(galaxies)
+velocity = []
+galaxy = []
 
 
+with open("galaxies.csv") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for i, line in enumerate(csv_reader):
+            if i !=0:
+                velocity.append(int(line[1]))
+                galaxy.append(line)
 
-######Q5 retry
+# print(velocity)
+# print(galaxy)
 
-with open('galaxies.csv')
+max_velocity = max(velocity)
+# print(max_velocity)
 
+min_velocity = min(velocity)
+# print(min_velocity)
 
-with open('galaxies.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    slow = [None, float('inf')]
-    fast = [None, 0]
-    for i, row in enumerate(csv_reader):
-        if i != 0:
-            if int(row[1]) < slow[1]:
-                slow =  [row[0], int(row[1])]
-            if int(row[1])> fast[1]:
-                fast = [row[0], int(row[1])]
-    print(f'Galaxy {slow[0]} has a min velocity of {slow[1]}km/sec.')
-    print(f'Galaxy {fast[0]} has a max velocity of {fast[1]}km/sec.')
+for line in galaxy:
+    if str(min_velocity) in line[1]:
+        print(f"Galaxy {line[0]} has the min velocity of {line[1]}km/sec.")
+    if str(max_velocity) in line[1]:
+        print(f"Galaxy {line[0]} has the min velocity of {line[1]}km/sec.")
