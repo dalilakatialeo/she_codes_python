6521cb459ef7717d187cb44940f015c31efb7ea3
Dalila K Leo <dalilakatialeo@gmail.com>
2021-12-04 08:19:51 +1000

    Update while_loops_playground.py

--- loops/while_loops_playground.py
+++ loops/while_loops_playground.py
@@ -1,37 +1,46 @@
 
-#WHILE LOOPS - best used when we don't know how many times to repeat
+# #WHILE LOOPS - best used when we don't know how many times to repeat
 
-# count = 0
+# # count = 0
 
-# while count < 5: #while is a boolean operator
-#     print("hello")  # keeps going, because its always true! infinite loop
-#     # count = count + 1 #stops infinite loop!
-#     count +=1 # it keeps adding 1 to the result #same as above
+# # while count < 5: #while is a boolean operator
+# #     print("hello")  # keeps going, because its always true! infinite loop
+# #     # count = count + 1 #stops infinite loop!
+# #     count +=1 # it keeps adding 1 to the result #same as above
 
-guess = ""
+# guess = ""
 
-while guess != "a":
-    guess = input("Guess a letter: ")
-
-
-counter = 10
-
-while counter <= 10:
-    print(counter)
-    counter = counter +1
-
-#CHALLENGE - ASK THE USER TO CONTINUALLY ENTER NAMES UNTIL A BLANK STRING IS ENTERED
-
-name = "x"
-
-while name != "":
-    name = input("Please enter name: ")
+# while guess != "a":
+#     guess = input("Guess a letter: ")
 
 
-#CHALLENGE - ASK THE USER TO CONTINUALLY ENTER A PASSWORD UNTIL A PASSWORD THAT YOU SET HAS BEEN GIVEN
+# counter = 10
 
-password = ""
+# while counter <= 10:
+#     print(counter)
+#     counter = counter +1
 
-while password != "blueberry":
-    password= input("Guess my password: ")
+# #CHALLENGE - ASK THE USER TO CONTINUALLY ENTER NAMES UNTIL A BLANK STRING IS ENTERED
 
+# name = "x"
+
+# while name != "":
+#     name = input("Please enter name: ")
+
+
+# #CHALLENGE - ASK THE USER TO CONTINUALLY ENTER A PASSWORD UNTIL A PASSWORD THAT YOU SET HAS BEEN GIVEN
+
+# password = ""
+
+# while password != "blueberry":
+#     password= input("Guess my password: ")
+
+# ask user to enter numbers, stop when blank and return sum of numbers entered
+
+numbers = []
+number = input("Please enter number: ")
+
+while number != "":
+    numbers.append(int(number))
+    number = input("Please enter number: ")
+print(sum(numbers))
